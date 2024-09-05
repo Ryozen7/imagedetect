d8ac78b891cc0dbdb196034d6936e01988335234
philip.rocky <philip.rocky@renren-inc.com>
2024-09-05 18:41:30 +0800

    update: sidebar

--- src/components/common/side-bar.js
+++ src/components/common/side-bar.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import Button from './button';
 import { links } from '../constants/image-links.constants';
 import copy from '../../assets/copy.png';
 
 export default function SideBar() {
     const [ isCopied, setIsCopied ] = useState(false); 
-    
+    const [showTooltip, setShowTooltip] = useState({});
+
+    const onMouseEnter = (id) => setShowTooltip({show: true, id});
+    const onMouseLeave = () => setShowTooltip({show: false, id: null});
     const CopyMe = async( link ) => {
         try {
             await navigator.clipboard.writeText(link);
@@ -38,13 +40,13 @@ export default function SideBar() {
                     return (
                         <div key={i} className='w-full flex flex-row mt-4 relative justify-between items-between'>
                             
-                            <div className='opacity-0 hover:opacity-100 w-[180px] overflowX mb-4 text-xs font-normal absolute z-10'>
-                                <span>
+                            <div className='relative w-[85%]' onMouseOver={()=> onMouseEnter(i)} onMouseOut={onMouseLeave}>
+                                <div className={showTooltip.show && showTooltip.id === i ? 'flex w-full overflowX text-xs font-normal absolute z-[10] bg-gray-dark -top-[36px] p-[4px] text-white rounded-md' : 'hidden'}>
                                 {link}
-                                </span>
+                                </div>
                                 
+                             <div className='w-full truncate' >{link}</div>
                             </div>
-                             <div className='w-3/4 truncate'>{link}</div>
                             <Button
                                 addClassName={'w-[40px] z-20'}
                                 handleClick={() => CopyMe(link)}
